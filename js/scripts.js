0bf2094f250f440c92d2baeb0685a872176e885f
JessSkyWatcher21 <Jspring5@ewu.edu>
2022-05-03 13:18:12 -0700

    Update JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,35 @@
 // My Scripts
 
-
-// Data source from Webflow
+// Data source
 const url = "https://v1.nocodeapi.com/pmanikoth/webflow/wtzODovSIJsRXhZg";
 
 // Get data
-fetch(url)
-  .then( response  => response.json())
+ fetch(url)
+   .then( response  => response.json())
   .then( data  => {
-    // check-check: get one image
-    // Note: Webflow returns data in array called `items`
-    console.log(data.items[0].image.url);
+     // check-check: get one image
+     // Note: Webflow returns data in array called `items`
+     console.log(data.items[0].image.url);
+   console.log(data.items[6].name.url);
 
     // get container for data
-    const gallery = document.querySelector(".gallery");
+     const gallery = document.querySelector(".gallery");
 
-    //
-    data.items.forEach( student => {
+     //
+     data.items.forEach( student => {
+       
       
       // template
       const template = `
-          <figure>
-            <figcaption>Student</figcaption>
-            <img src="${student.image.url}" alt="Placeholder" />
-          </figure>
+           <figure>
+           <figcaption>"${data.name}"</figcaption>
+             <img src="${student.image.url}" alt="Placeholder" />
+           </figure>
        `;
+      
 
-      // insert EACH `student` record into container
-      gallery.insertAdjacentHTML("afterbegin", template);
+
+       gallery.insertAdjacentHTML("afterbegin", template);
     });
   });
+
